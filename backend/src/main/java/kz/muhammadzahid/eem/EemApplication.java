3d1a7b1ba7fc19c2ffb69a1d189a35ff3dd84d85
Atabek04 <otabek.shadimatov@gmail.com>
2025-04-09 19:16:35 +0500

    Implement authentication

---
+++ backend/src/main/java/kz/muhammadzahid/eem/EemApplication.java
@@ -0,0 +1,13 @@
+package kz.muhammadzahid.eem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kz/muhammadzahid/eem/EemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.muhammadzahid.eem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
